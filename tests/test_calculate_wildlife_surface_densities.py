91f79dc1e585fb38a1fed3fcfc923ac91a2f68d3
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-03-30 11:56:30 -0700

    🧪 Add failing test for tecolote

--- tests/test_calculate_wildlife_surface_densities.py
+++ tests/test_calculate_wildlife_surface_densities.py
@@ -5,18 +5,32 @@ import json
 import numpy as np
 
 
-f = open("/workdir/tests/data/aves_density.json")
-
-expected = json.load(f)
-expected = {
-    k: [np.nan if elemento == "" else elemento for elemento in v] for k, v in expected.items()
-}
+def read_expected_density(path):
+    f = open(path)
+    expected = json.load(f)
+    return {
+        k: [np.nan if elemento == "" else elemento for elemento in v] for k, v in expected.items()
+    }
 
 
 def test_calculate_surface_densities_by_group():
     data_transects_path = "tests/data/transectos_fauna_nativa_isla_clarion_oct_2022.csv"
     data_transects = pd.read_csv(data_transects_path)
+
+    group = "Reptil"
+    obtained = calculate_surface_densities_by_group(group, data_transects)
+    assert isinstance(obtained, dict)
+    expected = read_expected_density("/workdir/tests/data/reptil_density.json")
+    assert obtained == expected
+
     group = "Ave"
     obtained = calculate_surface_densities_by_group(group, data_transects)
     assert isinstance(obtained, dict)
+    expected = read_expected_density("/workdir/tests/data/aves_density.json")
+    assert obtained == expected
+
+    group = "Tecolote"
+    obtained = calculate_surface_densities_by_group(group, data_transects)
+    assert isinstance(obtained, dict)
+    expected = read_expected_density("/workdir/tests/data/tecolote_density.json")
     assert obtained == expected
