574dffcf2bbde8914d65902bb6fd264dc6739a8f
Mario Villasante <mario.villasante@islas.org.mx>
2023-04-03 16:33:15 -0700

    🏹👾 Hunt mutants

--- tests/test_calculate_wildlife_surface_densities.py
+++ tests/test_calculate_wildlife_surface_densities.py
@@ -1,4 +1,8 @@
-from rabbit_clarion import calculate_surface_densities_by_group
+from rabbit_clarion import (
+    calculate_surface_densities_by_group,
+    check_array,
+    Native_Group_Parameters,
+)
 
 import pandas as pd
 import json
@@ -13,10 +17,11 @@ def read_expected_density(path):
     }
 
 
-def test_calculate_surface_densities_by_group():
-    data_transects_path = "tests/data/transectos_fauna_nativa_isla_clarion_oct_2022.csv"
-    data_transects = pd.read_csv(data_transects_path)
+data_transects_path = "tests/data/transectos_fauna_nativa_isla_clarion_oct_2022.csv"
+data_transects = pd.read_csv(data_transects_path)
 
+
+def test_calculate_surface_densities_by_group():
     group = "Reptil"
     obtained = calculate_surface_densities_by_group(group, data_transects)
     assert isinstance(obtained, dict)
@@ -35,3 +40,23 @@ def test_calculate_surface_densities_by_group():
     assert isinstance(obtained, dict)
     expected = read_expected_density("/workdir/tests/data/tecolote_density.json")
     np.testing.assert_equal(obtained, expected)
+
+
+def test_Native_Group_Parameters():
+    grupo = "Ave"
+    bird_parameters = Native_Group_Parameters(grupo, data_transects)
+    default_length = 300
+    assert bird_parameters.length == default_length
+    assert bird_parameters.group == grupo
+
+
+def test_check_array():
+    empty_array = np.array([])
+    obtained = check_array(empty_array)
+    expected = "NA"
+    assert obtained == expected
+
+    array = np.array([1, 2, 3])
+    obtained = check_array(array)
+    expected = 1
+    assert obtained == expected
