d4873f5fdaf8abec31087f3fa5f91850b03d98e9
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-06 20:46:46 +0000

    ♻️  Refactor

--- tests/test_calculate_estimation_by_vegetation.py
+++ tests/test_calculate_estimation_by_vegetation.py
@@ -6,9 +6,8 @@ import pandas as pd
 def test_separate_vegetations_enums():
     transect_densities_df = pd.read_csv("tests/data/df_to_test_separate_vegetations_enums.csv")
     obtained = separate_vegetations_enums(transect_densities_df)
-    expected_shape = (6, 5)
+    expected_shape = (7, 5)
     assert obtained.shape == expected_shape
     same_columns = transect_densities_df.columns[:-1]
-    print(same_columns)
     expected = transect_densities_df.loc[0, same_columns]
     assert (obtained.loc[1, same_columns] == expected).all()
