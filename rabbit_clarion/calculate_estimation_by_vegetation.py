fd522ed50cee9d74de2e500da8ae7ed5e707fb22
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-06 21:34:46 +0000

    ♻️  Refactor

--- rabbit_clarion/calculate_estimation_by_vegetation.py
+++ rabbit_clarion/calculate_estimation_by_vegetation.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 
 def separate_vegetations_enums(df):
-    df = df.dropna(subset=["Tipo_vegetacion"])
-    df = df.reset_index(drop=True).fillna(0)
+    df.dropna(subset=["Tipo_vegetacion"], inplace=True)
+    df.reset_index(drop=True).fillna(0, inplace=True)
     df["Tipo_vegetacion"] = df["Tipo_vegetacion"].str.split("/")
-    df = df.explode("Tipo_vegetacion").reset_index(drop=True)
-    return df
+    return df.explode("Tipo_vegetacion").reset_index(drop=True)
