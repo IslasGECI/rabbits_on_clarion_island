baf374cf55a35d946ceea5df834bb26abcdeb55b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-06 20:42:32 +0000

    ♻️  Refactor

--- rabbit_clarion/calculate_estimation_by_vegetation.py
+++ rabbit_clarion/calculate_estimation_by_vegetation.py
@@ -6,11 +6,13 @@ def separate_vegetations_enums(df):
     df = df.reset_index(drop=True).fillna(0)
     new_df = pd.DataFrame(columns=df.columns.values)
     for i in range(len(df)):
-        data_copy = df.loc[i].copy(deep=True)
-        if "/" in data_copy["Tipo_vegetacion"]:
-            vegetal_types = data_copy["Tipo_vegetacion"].split("/")
-            data_copy.Tipo_vegetacion = vegetal_types[0]
-            new_df = new_df._append(data_copy)
-            data_copy.Tipo_vegetacion = vegetal_types[1]
-        new_df = new_df._append(data_copy)
+        data_copy = df.loc[[i]].copy(deep=True)
+        if "/" in data_copy.iloc[0]["Tipo_vegetacion"]:
+            vegetal_types = data_copy.iloc[0]["Tipo_vegetacion"].split("/")
+            for veg in vegetal_types:
+                temp = data_copy.copy(deep=True)
+                temp.iloc[0, temp.columns.get_loc("Tipo_vegetacion")] = veg
+                new_df = pd.concat([new_df, temp], ignore_index=True)
+        else:
+            new_df = pd.concat([new_df, data_copy], ignore_index=True)
     return new_df.reset_index(drop=True)
--- tests/test_calculate_estimation_by_vegetation.py
+++ tests/test_calculate_estimation_by_vegetation.py
@@ -6,5 +6,9 @@ import pandas as pd
 def test_separate_vegetations_enums():
     transect_densities_df = pd.read_csv("tests/data/df_to_test_separate_vegetations_enums.csv")
     obtained = separate_vegetations_enums(transect_densities_df)
-    expected_rows = 6
-    assert len(obtained) == expected_rows
+    expected_shape = (6, 5)
+    assert obtained.shape == expected_shape
+    same_columns = transect_densities_df.columns[:-1]
+    print(same_columns)
+    expected = transect_densities_df.loc[0, same_columns]
+    assert (obtained.loc[1, same_columns] == expected).all()
