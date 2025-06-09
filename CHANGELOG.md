059d9f8b50c33c6c7a383cf8190bd9a35df8c121
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-09 22:02:12 +0000

    🆙 upgrade minor number version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Removed
 
+## [1.2.0] - 2025-06-09
+### Added
+- function `get_data_densities()`
+
 ## [1.1.0] - 2025-06-06
 ### Added
 - function `separate_vegetations_enums()`
--- rabbit_clarion/__init__.py
+++ rabbit_clarion/__init__.py
@@ -1,5 +1,5 @@
 """Module used in the repo conejos_clarion"""
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 from rabbit_clarion.calculate_wildlife_surface_densities import *  # noqa
-from rabbit_clarion.calculate_estimation_by_vegetation import separate_vegetations_enums  # noqa
+from rabbit_clarion.calculate_estimation_by_vegetation import get_data_densities, separate_vegetations_enums  # noqa
