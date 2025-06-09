707f9887f6e5d56e3d8bf51525380296f9663120
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-09 22:06:15 +0000

    🎨 Format with black

--- rabbit_clarion/__init__.py
+++ rabbit_clarion/__init__.py
@@ -2,4 +2,7 @@
 
 __version__ = "1.2.0"
 from rabbit_clarion.calculate_wildlife_surface_densities import *  # noqa
-from rabbit_clarion.calculate_estimation_by_vegetation import get_data_densities, separate_vegetations_enums  # noqa
+from rabbit_clarion.calculate_estimation_by_vegetation import (
+    get_data_densities,
+    separate_vegetations_enums,
+)  # noqa
