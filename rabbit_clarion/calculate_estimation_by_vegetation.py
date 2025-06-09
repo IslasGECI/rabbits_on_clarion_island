a846c64d10e101ed5b6b3f318c60b3b33af12ffa
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-09 20:38:01 +0000

    ✅ Pass tests

--- rabbit_clarion/calculate_estimation_by_vegetation.py
+++ rabbit_clarion/calculate_estimation_by_vegetation.py
@@ -1,5 +1,30 @@
+import pandas as pd
+
+
 def separate_vegetations_enums(df):
     df.dropna(subset=["Tipo_vegetacion"], inplace=True)
     df.reset_index(drop=True).fillna(0, inplace=True)
     df["Tipo_vegetacion"] = df["Tipo_vegetacion"].str.split("/")
     return df.explode("Tipo_vegetacion").reset_index(drop=True)
+
+
+def get_data_densities(data_path, data_areas_path):
+    data_densities = pd.read_csv(data_path)
+    data_areas = pd.read_csv(data_areas_path)
+    data_densities = add_missing_vegetal_types(data_densities, data_areas)
+    return data_densities
+
+
+def add_missing_vegetal_types(df, df_areas):
+    vegetal_types = df.Tipo_vegetacion.unique()
+    all_vegetal_types = df_areas.Tipo_de_vegetacion.unique()
+    missing_types = list(set(all_vegetal_types) - set(vegetal_types))
+    data_copy = df.loc[0].copy(deep=True)
+    data_copy.Densidad = 0
+    data_copy.Estimacion = 0
+    print(missing_types)
+    for vegetal_type in missing_types:
+        data_copy.Tipo_vegetacion = vegetal_type
+        print(data_copy)
+        df = df._append(data_copy)
+    return df.reset_index(drop=True)
