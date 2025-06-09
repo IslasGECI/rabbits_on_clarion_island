0eab63f9baf40730505418997411c9c3900bede6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-09 20:34:57 +0000

    🛑🧪 Fail tests

--- tests/test_calculate_estimation_by_vegetation.py
+++ tests/test_calculate_estimation_by_vegetation.py
@@ -1,4 +1,7 @@
-from rabbit_clarion.calculate_estimation_by_vegetation import separate_vegetations_enums
+from rabbit_clarion.calculate_estimation_by_vegetation import (
+    separate_vegetations_enums,
+    get_data_densities,
+)
 
 import pandas as pd
 
@@ -11,3 +14,13 @@ def test_separate_vegetations_enums():
     same_columns = transect_densities_df.columns[:-1]
     expected = transect_densities_df.loc[0, same_columns]
     assert (obtained.loc[1, same_columns] == expected).all()
+
+
+def test_get_data_densities():
+    data_path = "tests/data/rabbit_densities_for_tests.csv"
+    data_areas_path = "tests/data/vegetal_types_for_tests.csv"
+    obtained = get_data_densities(data_path, data_areas_path)
+    obtained_vegetation_types = set(obtained["Tipo_vegetacion"])
+    data_areas = pd.read_csv(data_areas_path)
+    expected_vegetation_types = set(data_areas)
+    assert obtained_vegetation_types == expected_vegetation_types
